43586c2158c3b72c074b7c3e364d7e88b1636e8d
bombo89 <53379021+bombo89@users.noreply.github.com>
2021-07-05 17:57:52 +0200

    Add files via upload
    A small algorithm that calculates greatest common denominator.

---
+++ LearnedInUdemy/Algorithms/Algorithms/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GreatestCommonDenominatior(24, 4));
+        }
+
+        static int GreatestCommonDenominatior(int a, int b)
+        {
+            int remainder;
+
+            while ( b != 0 )
+            {
+                remainder = a % b;
+
+                a = b;
+                b = remainder;
+            }
+
+            return a;
+        }
+    }
+}
